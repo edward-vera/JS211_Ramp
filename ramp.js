ec298d679246669accb824ba7d2122129fbb07b3
Chris Henry <chrislouishenry@gmail.comgit statuscd~cd>
2022-08-07 18:45:58 -0500

    initial commit

--- ramp.js
+++ ramp.js
@@ -1,29 +1,68 @@
-console.log(`Find the number of ramp numbers that are less than the number 99999`)
 
 function range(start, end, step = 1) {
     const len = Math.floor((end - start) / step) + 1
-    return Array(len).fill(end - start).map((_, idx) => start + (idx * step))
-};
-const result = range(1, 99999);
-console.log(result)
+    console.log(`len`, len);
+    const result = Array(len).fill(end - start).map((_, idx) => start + (idx * step));
+    console.log('result:', result);
 
-console.log(`Create a function that identifies ramp numbers and loops through the entire object`)
 
-function rampNumber(len) {
-    let magicNumber = len.toString().split("").map(Number);
-    let rampCount = 0;
-    for (i = 0; i < len; i++) {
-        if (magicNumber[i] > magicNumber[i - 1]) {
-            return false;
-        } else {
-            rampCount += 1;
+    const endResult = [];
+    for (let i = 0; i < result.length; i++) {
+        // console.log('u:', result[i]);
+        let number =  result[i].toString()
+        for (let j = 0; j < number.length; j++) {
+            if (i === number[j].length-1) {
+                // console.log("I'm the last element");
+
+                if (number[j] >=  number[j - 1]) {
+                    console.log(`J Last neighbor number`, number[j]);
+                    endResult.push(number[j])
+                }
+            } else {
+                if (number[j] <= number[j + 1]) {
+                    console.log(`J neighbor numbers`, number[j], number[j + 1]);
+                    endResult.push(number[j])
+                }
+            }
         }
     }
-console.log(rampCount)
-    return rampCount;
+    console.log(`endResult:`, endResult);
+
+
+
+    return result;
+};
+
+function rampNumber(number) {
+    let magicNumber = number.toString().split("").map(Number);
+    console.log(`len 2nd`, number)
+    console.log(`magicNumber`, magicNumber);
+    const resultArr = [];
+    if (number <= 99999){
+        for (let i = 0; i <= magicNumber.length; i++) {
+            if (i === magicNumber.length-1) {
+                // console.log("I'm the last element");
+                if (magicNumber[i] >= magicNumber[i - 1]) {
+                    console.log(`Last neighbor number`, magicNumber[i]);
+                    resultArr.push(magicNumber[i])
+                }
+            } else {
+                if (magicNumber[i] <= magicNumber[i + 1]) {
+                    console.log(`neighbor numbers`, magicNumber[i], magicNumber[i + 1]);
+                    resultArr.push(magicNumber[i])
+                }
+            }
+        }
+    }
+    console.log(`resultArr`, resultArr);
+    if (resultArr.length === magicNumber.length){
+        range(1, number);
+    } else {
+        return false;
+    }
 }
 
-console.log(rampNumber(99999))
+console.log(rampNumber(12))
 
 console.log(`Remove non-Ramp numbers from all identified ramp numbers`)
 
